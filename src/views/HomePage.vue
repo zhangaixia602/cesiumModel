dd266df5f6e09c947e09cddbf1d92d3371e318b4
zhangaixia602 <zhangaixia602@163.com>
2022-04-27 10:13:30 +0800

    关闭焦点弹出框

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,6 +16,8 @@
 				fullscreenButton: false,
 				creditsDisplay: false,
 				vrButton: false,
+				selectionIndicator: false,//关闭绿色框
+				 infoBox:false//关闭焦点弹出框
 				// skyBox:new Cesium.SkyBox({
 				//     sources : {
 				//         positiveX : '/static/px.jpg',
@@ -30,14 +32,17 @@
 			viewer._cesiumWidget._creditContainer.style.display = "none";
 			viewer.baseLayerPicker.viewModel.selectedImagery = viewer.baseLayerPicker.viewModel.imageryProviderViewModels[
 				6];
-			      let redBox = viewer.entities.add({
-			          name : 'Red box with black outline',
-			          position:Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706),
-			          model : {
-			              uri : '/static/cityhome.glb'
-			          }
-			      });
+			let redBox = viewer.entities.add({
+				name: 'Red box with black outline',
+				position: Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706),
+				model: {
+					uri: '/static/cityhome.glb'
+				}
+			});
+			//动态跟踪实物
 			viewer.trackedEntity = redBox;
+			//限制缩放
+			viewer.scene.screenSpaceCameraController.maximumZoomDistance = 2500;
 			// let palaceTileset = new Cesium.Cesium3DTileset({
 			// 	url: '/static/tileset.json'
 			// })
